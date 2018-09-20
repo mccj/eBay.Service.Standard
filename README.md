f92eb5e30c12ffca50bbdb80be006ff31459958f
mccj <mccj@MCCJ-PC>
2018-09-21 00:15:49 +0800

    -

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # eBay.Service.Standard
+
+
+Use the eBay SDK for .NET to simplify common development tasks like logging, error handling, and authentication.
--- eBay.Service.Standard/Util/SdkUtility.cs
+++ eBay.Service.Standard/Util/SdkUtility.cs
@@ -9,9 +9,6 @@
 #endregion
 
 #region Namespaces
-using System;
-using System.Web;
-using System.Collections;
 using eBay.Service.Core.Sdk;
 using System.Runtime.InteropServices;
 #endregion
@@ -19,39 +16,39 @@ using System.Runtime.InteropServices;
 namespace eBay.Service.Util
 {
 
-	/// <summary>
-	/// 
-	/// </summary>
-	[ClassInterface(ClassInterfaceType.AutoDispatch)]
-	public class SdkUtility
-	{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ClassInterface(ClassInterfaceType.AutoDispatch)]
+    public class SdkUtility
+    {
 
-		#region Static Methods
-		/// <summary>
-		/// Launches the sign in page for using the  Authentication &amp; Authorization feature.
-		/// </summary>
-		/// <param name="Context">The <see cref="ApiContext"/> which defines the SignInUrl and RuName.</param>
-		/// <param name="SessionID">The SessionID which is used by <see cref="eBay.Service.Call.FetchTokenCall"/> to retrieve the token.</param>
-		public static void LaunchSignInPage(ApiContext Context, string SessionID)
-		{
-			if(Context == null)
-				throw new SdkException("Please specify the Context.", new System.ArgumentNullException());
-			
-			if(Context.SignInUrl == null || Context.SignInUrl.ToString().Length == 0)
-				throw new SdkException("Please specify the SignInUrl in the Context object.", new System.ArgumentNullException());
+        #region Static Methods
+        /// <summary>
+        /// Launches the sign in page for using the  Authentication &amp; Authorization feature.
+        /// </summary>
+        /// <param name="Context">The <see cref="ApiContext"/> which defines the SignInUrl and RuName.</param>
+        /// <param name="SessionID">The SessionID which is used by <see cref="eBay.Service.Call.FetchTokenCall"/> to retrieve the token.</param>
+        public static void LaunchSignInPage(ApiContext Context, string SessionID)
+        {
+            if (Context == null)
+                throw new SdkException("Please specify the Context.", new System.ArgumentNullException());
 
-			if (Context.RuName == null || Context.RuName.Length == 0)
-				throw new SdkException("Please specify a RuName.", new System.ArgumentNullException());
+            if (Context.SignInUrl == null || Context.SignInUrl.ToString().Length == 0)
+                throw new SdkException("Please specify the SignInUrl in the Context object.", new System.ArgumentNullException());
 
-			// Go to the page.
-			string finalUrl = Context.SignInUrl + "&RuName=" + Context.RuName;
-			if(SessionID != null && SessionID.Length > 0 )
-				finalUrl += "&SessID=" + HttpUtility.UrlEncode(SessionID);
+            if (Context.RuName == null || Context.RuName.Length == 0)
+                throw new SdkException("Please specify a RuName.", new System.ArgumentNullException());
 
+            // Go to the page.
+            string finalUrl = Context.SignInUrl + "&RuName=" + Context.RuName;
+            if (SessionID != null && SessionID.Length > 0)
+                finalUrl += "&SessID=" + System.Net.WebUtility.UrlEncode(SessionID);
+            //System.Web.HttpUtility.UrlEncode()
             System.Diagnostics.Process.Start(finalUrl);
-		}
-		#endregion
-	}
+        }
+        #endregion
+    }
 }
 
 
